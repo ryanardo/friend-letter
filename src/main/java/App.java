c7eb42f88c51466a4d38f4286df5f00bbe6560e7
Ryan G Ferris <ryanglenferris@gmail.com>
2018-01-07 21:06:44 -0800

    html templates
    Create HTML templates with handlebar.js

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,25 @@
-import static spark.Spark.get;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/", (request, response) -> "Hello Friend!");
+        staticFileLocation("/public");
+        // HELLO FROM AFAR
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(new HashMap(), "hello.hbs"); //TELLS PROGRAM WHICH LINE TO RENDER.
+        }, new HandlebarsTemplateEngine());
+
+        // FAVORITE PHOTOS
+        get("/favorite_photos", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs"); //TELLS PROGRAM WHICH LINE TO RENDER.
+        }, new HandlebarsTemplateEngine());
+
     }
 }
